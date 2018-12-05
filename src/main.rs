64171911c5f808f9ee5e51961fad68ed9e904292
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2018-12-05 13:26:43 +0100

    Day05: Part02
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+extern crate char_iter;
 use std::fs::File;
 use std::io::prelude::*;
+use std::cmp;
 
 fn part01(list: &String) {
     let mut last_c;
@@ -23,6 +25,48 @@ fn part01(list: &String) {
     println!("Part01 result = {}", new_string.len());
 }
 
+fn remove_char(list: &String, fc: char) -> String {
+
+    let mut res = String::new();
+    for c in list.chars() {
+        if c.to_ascii_lowercase() != fc {
+            res.push(c);
+        }
+    }
+    res
+}
+
+fn react_full(list: &String) -> String {
+    let mut last_c;
+    let mut new_string = String::new();
+
+    for c in list.chars() {
+        if new_string.is_empty() {
+            new_string.push(c);
+        } else {
+            last_c = new_string.chars().last().unwrap();
+            if eq_opposite_case(c, last_c) {
+                new_string.pop();
+            } else {
+                if c != '\n' {
+                    new_string.push(c);
+                }
+            }
+        }
+    }
+    new_string
+}
+
+fn part02(list: &String) {
+    let mut min = std::usize::MAX;
+
+    for c in char_iter::new('a', 'z') {
+        let removed = remove_char(&list, c);
+        min = cmp::min(min, react_full(&removed).len());
+    }
+    println!("Part02 result = {}", min);
+}
+
 fn eq_opposite_case(this: char, that: char) -> bool {
     this.eq_ignore_ascii_case(&that) && (
         (this.is_lowercase() && that.is_uppercase()) ||
@@ -36,4 +80,5 @@ fn main() {
     f.read_to_string(&mut line).expect("Could not read file");
 
     part01(&line);
+    part02(&line);
 }
